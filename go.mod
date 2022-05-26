9965a4434ad2b2e561ba9842c58ce5a9681f3e00
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-26 20:40:50 +0900

    feat: 認証 -> 保存 -> APIアクセスまでの流れを仮実装

--- go.mod
+++ go.mod
@@ -2,16 +2,18 @@ module github.com/arrow2nd/nekome
 
 go 1.18
 
-require github.com/g8rswimmer/go-twitter/v2 v2.0.1
+require (
+	github.com/g8rswimmer/go-twitter/v2 v2.0.1
+	github.com/goccy/go-yaml v1.9.5
+	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5
+)
 
 require (
 	github.com/fatih/color v1.13.0 // indirect
-	github.com/goccy/go-yaml v1.9.5 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
-	golang.org/x/oauth2 v0.0.0-20220411215720-9780585627b5 // indirect
 	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df // indirect
 	google.golang.org/appengine v1.6.6 // indirect
--- main.go
+++ main.go
@@ -1,19 +1,72 @@
 package main
 
 import (
-	"fmt"
 	"log"
 
+	"github.com/arrow2nd/nekome/api"
+	"github.com/arrow2nd/nekome/config"
 	"github.com/arrow2nd/nekome/oauth"
+	"golang.org/x/oauth2"
 )
 
-func main() {
-	client := oauth.New()
+var (
+	client *api.API
+	conf   *config.Config
+)
 
+func init() {
+	client = api.New()
+	conf = config.New()
+}
+
+func main() {
+	ok, err := conf.LoadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if !ok {
+		initConfig()
+	}
+
+	userName := conf.Settings.MainUser
+	token, err := conf.Cred.Get(userName)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client.SetUser(userName, token)
+	client.SetTokenRefreshCallback(handleTokenRefresh)
+
+	client.AuthUserLookup()
+}
+
+func initConfig() {
 	token, err := client.Auth()
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Println(token)
+	conf.Cred.Write("test", token)
+	conf.Settings.MainUser = "test"
+
+	if err := conf.SaveAll(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func handleTokenRefresh(rawToken *oauth2.Token) error {
+	userName := client.UserName
+	token := &oauth.Token{
+		AccessToken:  rawToken.AccessToken,
+		RefreshToken: rawToken.RefreshToken,
+		Expiry:       rawToken.Expiry,
+	}
+
+	client.SetUser(userName, token)
+
+	conf.Cred.Write(userName, token)
+	conf.SaveCred()
+
+	return nil
 }
