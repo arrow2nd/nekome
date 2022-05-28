8eaa34bc756a7d2a724ba1fca158742d1eb33320
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-28 16:15:42 +0900

    feat(api): ユーザのメンションタイムラインの取得

--- api/timeline.go
+++ api/timeline.go
@@ -6,31 +6,60 @@ import (
 	"github.com/g8rswimmer/go-twitter/v2"
 )
 
+var (
+	tweetFiled = []twitter.TweetField{
+		twitter.TweetFieldCreatedAt,
+		twitter.TweetFieldAuthorID,
+		twitter.TweetFieldPublicMetrics,
+		twitter.TweetFieldEntities,
+	}
+	userFiled = []twitter.UserField{
+		twitter.UserFieldUserName,
+		twitter.UserFieldName,
+	}
+)
+
 // UserTimeline ユーザタイムラインを取得
-func (a *API) UserTimeline(userID string) ([]*twitter.TweetObj, error) {
+func (a *API) UserTimeline(userID string, results int) ([]*twitter.TweetObj, error) {
 	client, err := a.newClient(a.CurrentUser.Token)
 	if err != nil {
 		return nil, err
 	}
 
 	opts := twitter.UserTweetTimelineOpts{
-		TweetFields: []twitter.TweetField{
-			twitter.TweetFieldCreatedAt,
-			twitter.TweetFieldAuthorID,
-			twitter.TweetFieldPublicMetrics,
-			twitter.TweetFieldEntities,
-		},
-		UserFields: []twitter.UserField{
-			twitter.UserFieldUserName,
-			twitter.UserFieldName,
-		},
+		TweetFields: tweetFiled,
+		UserFields:  userFiled,
 		Expansions: []twitter.Expansion{
 			twitter.ExpansionAuthorID,
 		},
-		MaxResults: 25,
+		MaxResults: results,
 	}
 
 	timeline, err := client.UserTweetTimeline(context.Background(), userID, opts)
+	if err != nil {
+		return nil, err
+	}
+
+	return timeline.Raw.Tweets, nil
+}
+
+// UserMentionTimeline ユーザのメンションタイムラインを取得
+func (a *API) UserMentionTimeline(userID string, results int) ([]*twitter.TweetObj, error) {
+	client, err := a.newClient(a.CurrentUser.Token)
+	if err != nil {
+		return nil, err
+	}
+
+	opts := twitter.UserMentionTimelineOpts{
+		TweetFields: tweetFiled,
+		UserFields:  userFiled,
+		Expansions: []twitter.Expansion{
+			twitter.ExpansionAuthorID,
+		},
+		MaxResults: results,
+	}
+
+	timeline, err := client.UserMentionTimeline(context.Background(), userID, opts)
 	if err != nil {
 		return nil, err
 	}
