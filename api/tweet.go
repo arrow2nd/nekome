56c2994ea30b71cabd8170a458fefd15d8ec304a
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-29 21:37:33 +0900

    feat(api): ツイートの投稿・削除

---
+++ api/tweet.go
@@ -0,0 +1,34 @@
+package api
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/g8rswimmer/go-twitter/v2"
+)
+
+// PostTweet ツイートを投稿
+func (a *API) PostTweet(text string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	req := twitter.CreateTweetRequest{
+		Text: text,
+	}
+
+	if _, err := client.CreateTweet(context.Background(), req); err != nil {
+		return fmt.Errorf("post tweet error: %v", err)
+	}
+
+	return nil
+}
+
+// DeleteTweet ツイートを削除
+func (a *API) DeleteTweet(tweetId string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.DeleteTweet(context.Background(), tweetId); err != nil {
+		return fmt.Errorf("delete tweet error: %v", err)
+	}
+
+	return nil
+}
