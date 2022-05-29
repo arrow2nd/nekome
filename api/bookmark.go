1a87ad3aaf29d3fe939bbaedec0615a00dd4624d
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-29 21:01:18 +0900

    feat(api): ブックマークの追加・削除

--- api/bookmark.go
+++ api/bookmark.go
@@ -26,3 +26,26 @@ func (a *API) FetchBookmarkTweets() ([]*twitter.TweetObj, error) {
 
 	return result.Raw.Tweets, nil
 }
+
+// AddBookmark ツイートをブックマークに追加
+// FIXME: Invalid Request:One or more parameters to your request was invalid
+func (a *API) AddBookmark(tweetID string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.AddTweetBookmark(context.Background(), a.CurrentUser.ID, tweetID); err != nil {
+		return fmt.Errorf("add bookmark: %v", err)
+	}
+
+	return nil
+}
+
+// DeleteBookmark ツイートをブックマークから削除
+func (a *API) DeleteBookmark(tweetID string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.RemoveTweetBookmark(context.Background(), a.CurrentUser.ID, tweetID); err != nil {
+		return fmt.Errorf("delete bookmark: %v", err)
+	}
+
+	return nil
+}
