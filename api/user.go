efb79e3a2782657906fdb26178262b298a7bacdd
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-30 10:09:47 +0900

    feat(api): UserNameからユーザ情報を取得

---
+++ api/user.go
@@ -0,0 +1,24 @@
+package api
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/g8rswimmer/go-twitter/v2"
+)
+
+// FetchUser UserNameからユーザ情報を取得
+func (a *API) FetchUser(userNames []string) ([]*twitter.UserObj, error) {
+	client := a.newClient(a.CurrentUser.Token)
+
+	opts := twitter.UserLookupOpts{
+		UserFields: userFields,
+	}
+
+	result, err := client.UserNameLookup(context.Background(), userNames, opts)
+	if err != nil {
+		return nil, fmt.Errorf("username lookup error: %v", err)
+	}
+
+	return result.Raw.Users, nil
+}
