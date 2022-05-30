aee08a72255404fcffae87202a6477b2b18fd8f0
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-30 10:04:47 +0900

    feat(api): エラーメッセージに発生個所を追加

---
+++ api/follow.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"context"
+	"fmt"
+)
+
+// Follow ユーザをフォロー
+func (a *API) Follow(userID string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.UserFollows(context.Background(), a.CurrentUser.ID, userID); err != nil {
+		return fmt.Errorf("user follow error: %v", err)
+	}
+
+	return nil
+}
+
+// UnFollow ユーザのフォローを解除
+func (a *API) UnFollow(userID string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.DeleteUserFollows(context.Background(), a.CurrentUser.ID, userID); err != nil {
+		return fmt.Errorf("user unfollow error: %v", err)
+	}
+
+	return nil
+}
