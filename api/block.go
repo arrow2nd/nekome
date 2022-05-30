8107d4fc284a10033fb57c762ebae713ecafe009
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-05-30 10:32:17 +0900

    feat(api): ユーザのブロック・アンブロック

---
+++ api/block.go
@@ -0,0 +1,28 @@
+package api
+
+import (
+	"context"
+	"fmt"
+)
+
+// Block ユーザをブロック
+func (a *API) Block(userID string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.UserBlocks(context.Background(), a.CurrentUser.ID, userID); err != nil {
+		return fmt.Errorf("user block error: %v", err)
+	}
+
+	return nil
+}
+
+// UnBlock ユーザのブロックを解除
+func (a *API) UnBlock(userID string) error {
+	client := a.newClient(a.CurrentUser.Token)
+
+	if _, err := client.DeleteUserBlocks(context.Background(), a.CurrentUser.ID, userID); err != nil {
+		return fmt.Errorf("user unblock error: %v", err)
+	}
+
+	return nil
+}
