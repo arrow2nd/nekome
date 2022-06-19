293882b2fbce8ef48b8aed0856057f9c0d700776
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-06-19 18:11:54 +0900

    feat(ui): ユーザページを仮実装

--- ui/page_user.go
+++ ui/page_user.go
@@ -10,38 +10,43 @@ import (
 
 type userPage struct {
 	*basePage
-	detail         *tview.TextView
-	tweetCount     *tview.TextView
-	followingCount *tview.TextView
-	followersCount *tview.TextView
-	user           *twitter.UserObj
+	detail *tview.TextView
+	// tweetCount     *tview.TextView
+	// followingCount *tview.TextView
+	// followersCount *tview.TextView
+	targetUserID string
+	user         *twitter.UserObj
 }
 
-func newUserPage() *userPage {
+func newUserPage(name, userID string) *userPage {
 	page := &userPage{
-		basePage:       newBasePage(),
-		detail:         tview.NewTextView(),
-		tweetCount:     newCountTextView(0xe06c75),
-		followingCount: newCountTextView(0xc678dd),
-		followersCount: newCountTextView(0x56b6c2),
-		user:           nil,
+		basePage: newBasePage(name),
+		detail:   tview.NewTextView(),
+		// tweetCount:     newCountTextView(0xe06c75),
+		// followingCount: newCountTextView(0xc678dd),
+		// followersCount: newCountTextView(0x56b6c2),
+		targetUserID: userID,
+		user:         nil,
 	}
 
 	page.detail.SetDynamicColors(true).
 		SetWrap(true)
 
-	countView := tview.NewFlex().
-		AddItem(page.tweetCount, 0, 1, false).
-		AddItem(page.followingCount, 0, 1, false).
-		AddItem(page.followersCount, 0, 1, false)
+	page.tweets.view.SetBorderPadding(1, 0, 0, 0)
+
+	// countView := tview.NewFlex().
+	// 	AddItem(page.tweetCount, 0, 1, false).
+	// 	AddItem(page.followingCount, 0, 1, false).
+	// 	AddItem(page.followersCount, 0, 1, false)
 
 	layout := tview.NewFlex().
 		SetDirection(tview.FlexRow).
-		AddItem(page.detail, 4, 1, false).
-		AddItem(countView, 1, 1, false).
-		AddItem(page.tweets.view, 0, 1, false)
+		AddItem(page.detail, 5, 1, false).
+		AddItem(page.tweets.view, 0, 1, true)
 
 	page.SetFrame(layout)
+
+	page.frame.SetInputCapture(page.handleKeyEvents)
 
 	return page
 }
@@ -58,21 +63,38 @@ func newCountTextView(color int32) *tview.TextView {
 }
 
 func (u *userPage) Load() {
-	u.setUserDetail("ユーザー名", "user", "[blue]following", "BIO", "place")
-	u.setCounts("10000", "10000", "10000")
+	shared.setStatus(u.name, "Loading...")
+
+	sinceID := u.tweets.getSinceID()
+	tweets, rateLimit, err := shared.api.FetchUserTimeline(u.targetUserID, sinceID, 25)
+	if err != nil {
+		shared.setErrorStatus(u.name, err.Error())
+	}
+
+	u.tweets.register(tweets)
+	u.tweets.draw()
+
+	if u.tweets.count != 0 {
+		u.setUserDetail(tweets[0].Author)
+	}
+
+	u.showLoadedStatus(rateLimit)
 }
 
-func (u *userPage) setUserDetail(name, userName, relation, bio, place string) {
+func (u *userPage) setUserDetail(user *twitter.UserObj) {
 	u.detail.Clear()
 
-	fmt.Fprintf(u.detail, "[white:-:b]%s\n", name)
-	fmt.Fprintf(u.detail, "[gray:-:i]@%s[-:-:-] %s\n", userName, relation)
-	fmt.Fprintf(u.detail, "[white:-:-]📄 : %s\n", bio)
-	fmt.Fprintf(u.detail, "📍 : %s", place)
+	fmt.Fprint(u.detail, createHeader(user, -1))
+	fmt.Fprintf(u.detail, "[white:-:-]%s\n", user.Description)
+	fmt.Fprintf(u.detail, " : %s\n", user.Location)
+	fmt.Fprintf(u.detail, " : %s\n", user.URL)
+	fmt.Fprintf(u.detail, "%d Tweets / %d Following / %d Followers", user.PublicMetrics.Tweets, user.PublicMetrics.Following, user.PublicMetrics.Followers)
+
+	// u.tweetCount.SetText(fmt.Sprintf("%d Tweets", user.PublicMetrics.Tweets))
+	// u.followingCount.SetText(fmt.Sprintf("%d Following", user.PublicMetrics.Following))
+	// u.followersCount.SetText(fmt.Sprintf("%d Followers", user.PublicMetrics.Followers))
 }
 
-func (u *userPage) setCounts(tweets, following, followers string) {
-	u.tweetCount.SetText(fmt.Sprintf("%s Tweets", tweets))
-	u.followingCount.SetText(fmt.Sprintf("%s Following", following))
-	u.followersCount.SetText(fmt.Sprintf("%s Followers", followers))
+func (u *userPage) handleKeyEvents(event *tcell.EventKey) *tcell.EventKey {
+	return handlePageKeyEvents(u, event)
 }
