6e47d3f9a1335b84a7eb787ddb89e5fb73b85df1
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-06-19 18:07:31 +0900

    refactor(api)：コメントアウトしていたコードを削除

--- api/error.go
+++ api/error.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/g8rswimmer/go-twitter/v2"
 )
@@ -9,29 +10,10 @@ import (
 func checkError(err error) error {
 	// レート制限
 	if rateLimit, has := twitter.RateLimitFromError(err); has {
+		log.Println(err)
 		t := rateLimit.Reset.Time().Local().Format("15:04:05")
 		return fmt.Errorf("Rate limit exceeded (Reset time: %s)", t)
 	}
-
-	//calloutError := &json.UnsupportedValueError{}
-	//decodeError := &twitter.ResponseDecodeError{}
-	//httpError := &twitter.HTTPError{}
-
-	//switch {
-	//case errors.As(err, &calloutError):
-	//	//
-	//	log.Println("callout")
-	//	return calloutError.Str
-	//case errors.As(err, &decodeError):
-	//	//
-	//	log.Println("decode")
-	//	return decodeError.Name
-	//case errors.As(err, &httpError):
-	//	//
-	//	return fmt.Sprintf("%d %s", httpError.StatusCode, httpError.Status)
-	//case err != nil:
-	//	return fmt.Sprintf("unknown: %s", err.Error())
-	//}
 
 	return err
 }
