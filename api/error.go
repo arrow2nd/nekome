ceef1d63ff2dbcdb45734ec25745261c044fb86f
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-06-21 21:44:08 +0900

    refactor(api): 不要なログ書き出しを削除

--- api/error.go
+++ api/error.go
@@ -2,15 +2,15 @@ package api
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/g8rswimmer/go-twitter/v2"
 )
 
 func checkError(err error) error {
+	// TODO: エラーハンドリングをもう少ししっかりやる
+
 	// レート制限
 	if rateLimit, has := twitter.RateLimitFromError(err); has {
-		log.Println(err)
 		t := rateLimit.Reset.Time().Local().Format("15:04:05")
 		return fmt.Errorf("Rate limit exceeded (Reset time: %s)", t)
 	}
