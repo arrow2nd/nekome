3532506b3d8eaf07d939a91f6f9f8633a2f3e497
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-06-24 17:49:20 +0900

    feat(app/cli): home, mention コマンドを追加

--- app/cli.go
+++ app/cli.go
@@ -12,6 +12,8 @@ import (
 func (a *App) getCommands() []string {
 	return []string{
 		"tweet",
+		"home",
+		"mention",
 		"user",
 		"list",
 		"search",
@@ -22,32 +24,33 @@ func (a *App) getCommands() []string {
 
 // ExecCmd : コマンドを実行
 func (a *App) ExecCmd(args []string) error {
-	// tweetコマンドのフラグを設定
+	// フラグを設定
+	f := flag.NewFlagSet("nekome", flag.ContinueOnError)
 	tweetFlag := flag.NewFlagSet("tweet", flag.ContinueOnError)
 
-	tweetFlag.AddFlag(&flag.Flag{
-		Name:      "quote",
-		Shorthand: "q",
-		Usage:     "Specify the ID of the tweet to quote",
-	})
-
-	tweetFlag.AddFlag(&flag.Flag{
-		Name:      "reply",
-		Shorthand: "r",
-		Usage:     "Specify the ID of the tweet to which you are replying",
-	})
+	f.BoolP("unfocus", "u", false, "")
+	tweetFlag.BoolP("quote", "q", false, "Specify the ID of the tweet to quote")
+	tweetFlag.BoolP("reply", "r", false, "Specify the ID of the tweet to which you are replying")
 
 	// 引数をパース
-	f := flag.NewFlagSet("nekome", flag.ContinueOnError)
-	f.Parse(args)
+	if err := f.Parse(args); err != nil {
+		return err
+	}
 
 	if f.NArg() == 0 {
 		return errors.New("command not found")
 	}
 
+	unfocus, _ := f.GetBool("unfocus")
+
+	// コマンドを解析
 	switch f.Arg(0) {
+	case "home", "h":
+		return a.view.AddPage(newTimelinePage(homeTL), !unfocus)
+	case "mention", "m":
+		return a.view.AddPage(newTimelinePage(mentionTL), !unfocus)
 	case "user", "u":
-		return a.openUserPage(f.Arg(1))
+		return a.openUserPage(f.Arg(1), !unfocus)
 	case "quit", "q":
 		a.quitApp()
 		return nil
@@ -57,7 +60,7 @@ func (a *App) ExecCmd(args []string) error {
 }
 
 // openUserPage : ユーザページを開く
-func (a *App) openUserPage(userName string) error {
+func (a *App) openUserPage(userName string, focus bool) error {
 	// ユーザの指定がないなら自分を指定
 	if userName == "" {
 		userName = shared.api.CurrentUser.UserName
@@ -66,7 +69,7 @@ func (a *App) openUserPage(userName string) error {
 	// @を除去
 	userName = strings.ReplaceAll(userName, "@", "")
 
-	return a.view.AddPage(newUserPage(userName), true)
+	return a.view.AddPage(newUserPage(userName), focus)
 }
 
 // quitApp : アプリを終了
