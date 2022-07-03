ac71bd4894ba0da48ea1f45f6ed2d7d8f9c28336
arrow2nd <44780846+arrow2nd@users.noreply.github.com>
2022-07-03 19:14:36 +0900

    refactor: 不要なコードを削除

--- main.go
+++ main.go
@@ -16,30 +16,3 @@ func main() {
 		log.ErrorExit(err.Error(), log.ExitCodeErrApp)
 	}
 }
-
-// func createNewCred() {
-// 	authUser, err := client.Auth(&conf.Settings.Feature.Consumer)
-// 	if err != nil {
-// 		log.ErrorExit(err.Error(), log.ExitCodeErrAuth)
-// 	}
-
-// 	conf.Cred.Write(authUser)
-// 	conf.Settings.Feature.MainUser = authUser.UserName
-
-// 	if err := conf.SaveAll(); err != nil {
-// 		log.ErrorExit(err.Error(), log.ExitCodeErrFileIO)
-// 	}
-// }
-
-// func login() error {
-// 	// ログインするユーザを取得
-// 	userName := conf.Settings.Feature.MainUser
-// 	user, err := conf.Cred.Get(userName)
-// 	if err != nil {
-// 		return err
-// 	}
-
-// 	client = api.New(&conf.Settings.Feature.Consumer, user)
-
-// 	return nil
-// }
